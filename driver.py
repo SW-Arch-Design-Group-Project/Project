59ae00aa10fe0e79f4f5603915736d88beecf69d
Kris P <knp283@msstate.edu>
2019-10-24 12:44:18 -0500

    Adding a base driver file for reference.

---
+++ driver.py
@@ -0,0 +1,20 @@
+def main():
+    print("Welcome to the Store!")
+    print()
+
+
+    #login and validation (if it doesn't return a boolean, change this to accept a username.)
+    valid = login()
+    while valid = False:
+        login()
+    
+    displayTable():    
+    
+
+    return
+
+def login():
+    return
+
+def displayTable():
+    return
