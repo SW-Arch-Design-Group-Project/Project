36ed1fc14ac79cd7d19fed4cc6643ba604590836
Mark-Hodge <33436232+Mark-Hodge@users.noreply.github.com>
2019-10-25 15:32:51 -0500

    Added home and user options

--- driver.py
+++ driver.py
@@ -6,21 +6,17 @@ def main():
 
     #Builds initial Database.
     createdatabase.buildDatabase()
-    
-    
+
+
 
     print("\n***************************")
     print("*  Welcome to the Store!  *")
     print("***************************\n")
 
-    
-
-
-    #login and validation (if it doesn't return a boolean, change this to accept a username.)
-
+    # login and validation (if it doesn't return a boolean, change this to accept a username.)
     login.ValidateUser()
 
-    displayTable()
+    session = homeScreen()
 
     # TODO This function may can be called within the displayTable() Function
     #      to allow the display table to update itself and take load off the main
@@ -33,24 +29,43 @@ def main():
     return
 
 
+def homeScreen():
+    conn = sqlite3.connect('store.db')
+    exitStore = 0
 
+    while (exitStore != 1):
+        try:
+            print("\nStore Options:\n1. View Inventory\n2. View Past Orders\n0. Exit Store")
+            userSelection = int(input("Select an option: "))
 
-def displayTable():
+            if (userSelection) == (1):
+                displayTable(conn)
+            elif (userSelection) == (2):
+                print("past orders\n")
+            elif (userSelection) == (0):
+                print("Goodbye\n")
+                exit()
+
+        except Exception as ex:
+            print("Please enter a store option.\n")
+
+    return conn
+
+def displayTable(conn):
 
     # TODO Add this function that will display the items from the Database that will be available
     #      add to cart.
 
-    conn = sqlite3.connect('store.db')
+    # conn = sqlite3.connect('store.db')
     cursor = conn.execute('SELECT * FROM Inventory GROUP BY Category')
 
-    print('{:<10s}{:<25s}{:<30s}{:<35s}{:<45s}{:<50s}'.format("Item ID", "Name", "Quantity", "Category", "Price", "Desc"))
+    print('{:<10s}{:<25s}{:<30s}{:<35s}{:<45s}{:<50s}'.format("\nItem ID", "Name", "Quantity", "Price", "Category", "Desc"))
     for row in cursor:
-        print('{:<10d}{:<25s}{:<30d}{:<35f}{:<45s}{:<50s}'.format(row[0], row[1], row[5],
+        print('{:<10d}{:<25s}{:<30d}{:<35.2f}{:<45s}{:<50s}'.format(row[0], row[1], row[5],
               row[3], row[4], row[2]))
+    print("\n")
 
     return
-
-
 
 
 
