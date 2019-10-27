d64cf231fc11cee0f04db894f15ec0380861f4da
Mark-Hodge <33436232+Mark-Hodge@users.noreply.github.com>
2019-10-27 17:00:56 -0500

    Fixed OrderHistory

--- driver.py
+++ driver.py
@@ -24,13 +24,13 @@ def userLogin():
 
 
 def homeScreen(userID):
-    print("\n---------------- Store Home ----------------\n")
     currentUser = userID
     conn = sqlite3.connect('store.db')
     exitStore = 0
 
     while (exitStore != 1):
         try:
+            print("\n---------------- Store Home ----------------")
             print("Store Options:\n1. View Inventory\n2. View Past Orders\n3. Build Cart\n9. Logout\n0. Exit Store\n")
             userSelection = int(input("Select an option: "))
 
@@ -58,22 +58,41 @@ def homeScreen(userID):
 
 
 def displayOrderHistory(conn, currentUser):
+    print("\n---------------- Order History ----------------\n")
+
     cursor = conn.execute("SELECT UserID FROM User WHERE Username = ?", (currentUser,))
-    for row in cursor:
-        userID = row[0]
-        print(type(userID))
-    print(userID)
+    data = cursor.fetchone()
+    if (data is None):
+        print("You are not a user")
 
-    cursor = conn.execute("SELECT * FROM PurchaseHistory WHERE UserID = ?", (userID,))
+    else:
+        userID = data[0]
+        hasOrders = True
 
-    print('{:<10s}{:>25s}{:>30s}{:>35s}{:>45s}'.format("\nPurchase ID", "Order ID", "User ID",
-                                                       "Total", "Credit Card"))
-    for row in cursor:
-        print('{:<10d}{:>25d}{:>30d}{:>35.2f}{:>45d}'.format(row[0], row[1], row[2],
-              row[3], row[4]))
+        cursor = conn.execute("SELECT * FROM PurchaseHistory WHERE UserID = ?", (userID,))
+        data = cursor.fetchone()
+
+        if (data is None):
+            print("You have no previous orders.\n")
+            hasOrders = False
+
+        else:
+            print('{:<10s}{:>25s}{:>30s}{:>35s}{:>45s}'.format("\nPurchase ID", "Order ID", "User ID",
+                                                               "Total", "Credit Card"))
+
+        while (hasOrders == True):
+            print('{:<10d}{:>25d}{:>30d}{:>35.2f}{:>45d}'.format(data[0], data[1], data[2],
+                  data[3], data[4]))
+            data = cursor.fetchone()
+            if (data is None):
+                break
+
+
 
 
 def displayTable(conn):
+    print("\n---------------- Store Inventory ----------------\n")
+
 
     # TODO Add this function that will display the items from the Database that will be available
     #      add to cart.
@@ -86,8 +105,6 @@ def displayTable(conn):
         print('{:<10d}{:<15s}{:<20d}{:<25.2f}{:<30s}{:<35s}'.format(row[0], row[1], row[5],
             row[3], row[4], row[2]))
     print("\n")
-
-    # addToCart()
 
     return
 
@@ -181,7 +198,9 @@ def reviewCart(currentUser, conn, cart):
 def confirmPurchase(currentUser, cart, conn):
 
     #Get UserID from currentUser
-    UserID = conn.execute("SELECT UserID FROM User WHERE Username = ?", (currentUser,))
+    cursor = conn.execute("SELECT UserID FROM User WHERE Username = ?", (currentUser,))
+    data = cursor.fetchone()
+    UserID = data[0]
 
     #Search database for user address, then prompt if not there
     cursor = conn.execute("SELECT Address FROM User WHERE UserID = ?", (UserID,))
@@ -203,19 +222,28 @@ def confirmPurchase(currentUser, cart, conn):
         VALUES (?)", (UserID,));
 
     #Select the most recent orderID for the user
-    OrderID = conn.execute("SELECT MAX(OrderID) FROM Orders WHERE UserID = ?", (UserID,))
+    cursor = conn.execute("SELECT MAX(OrderID) FROM Orders WHERE UserID = ?", (UserID,))
+    data = cursor.fetchone()
+    OrderID = data[0]
 
     #Insert the itemID's and their quantities into the OrderItems Table and update the Inventory table
     total = 0
     for item in cart:
         price = 0
-        ItemID = [item][0]
-        Quantity = [item][2]
-        price = conn.execute("SELECT Price FROM Inventory WHERE ItemID = ?", (ItemID,))
+        ItemID = item[0]
+        Quantity = item[2]
+        cursor = conn.execute("SELECT Price FROM Inventory WHERE ItemID = ?", (ItemID,))
+        data = cursor.fetchone()
+        price = data[0]
         total += price * Quantity
+
         conn.execute("INSERT INTO OrderItems (OrderID, ItemID, Quantity)\
             VALUES (?, ?, ?)", (OrderID, ItemID, Quantity,));
-        current_quantity = conn.execute("SELECT Quantity FROM Inventory WHERE ItemID = ?", (ItemID,))
+
+        cursor = conn.execute("SELECT Quantity FROM Inventory WHERE ItemID = ?", (ItemID,))
+        data = cursor.fetchone()
+        current_quantity = data[0]
+
         new_quantity = current_quantity - Quantity
         conn.execute("UPDATE Inventory SET Quantity = ? WHERE ItemID = ?", (new_quantity,ItemID,))
 
@@ -223,6 +251,7 @@ def confirmPurchase(currentUser, cart, conn):
     conn.execute("INSERT INTO PurchaseHistory (OrderID, UserID, Total, CreditCard)\
             VALUES (?, ?, ?, ?)", (OrderID, UserID, total, creditcard,));
 
+    conn.commit()
 
 # MAIN FUNCTION CALL
 if __name__ == "__main__":
